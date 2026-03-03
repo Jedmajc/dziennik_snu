022a8b1be76d273fead90ff8fcb4b5802d8a722e
Jędrzej Majchrzak <jedmajc@gmail.com>
2026-03-03 10:42:30 +0100

    Male poprawki  + bugfixes

--- app/src/main/java/com/example/dziennik_snu/MainActivity.kt
+++ app/src/main/java/com/example/dziennik_snu/MainActivity.kt
@@ -7,8 +7,11 @@ import android.os.Bundle
 import android.widget.Button
 import android.widget.EditText
 import android.widget.Toast
+import androidx.activity.enableEdgeToEdge
 import androidx.activity.viewModels
 import androidx.appcompat.app.AppCompatActivity
+import androidx.core.view.ViewCompat
+import androidx.core.view.WindowInsetsCompat
 import com.example.dziennik_snu.data.SleepEntry
 import com.example.dziennik_snu.ui.SleepViewModel
 import com.google.android.material.slider.Slider
@@ -41,7 +44,14 @@ class MainActivity : AppCompatActivity() {
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
+        enableEdgeToEdge()
         setContentView(R.layout.activity_main)
+
+        ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main)) { v, insets ->
+            val systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars())
+            v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom)
+            insets
+        }
 
         initializeViews()
         setupDatePicker()
